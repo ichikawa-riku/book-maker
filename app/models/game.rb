cb3bd0eacda48b1bae927f78ee32e409d772ed61
ichikawa-riku <owner@M15-108.local>
2019-03-26 18:26:34 +0900

    edit modles

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,10 @@
 class Game < ApplicationRecord
+  has_one :user, through: :owner
   has_one :owner
-  has_many :players
   accepts_nested_attributes_for :owner
+  has_many :players
   accepts_nested_attributes_for :players
+  has_many :bets
   @now = Time.zone.now
   scope :incoming,  -> { where('closed_at > ?', @now) }
-
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
   belongs_to :game, optional: true
+  has_many :bets
 end
